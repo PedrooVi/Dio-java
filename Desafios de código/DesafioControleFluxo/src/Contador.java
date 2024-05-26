8b9cfe9d2b1d41c3a020dd8295474fbb5ef9fe96
PedrooVi <pedroviniciuscorrea@outlook.com>
2024-05-26 00:17:23 -0300

    modified:   "Desafios de c\303\263digo/DesafioControleFluxo/src/Contador.java"

--- Desafios de código/DesafioControleFluxo/src/Contador.java
+++ Desafios de código/DesafioControleFluxo/src/Contador.java
@@ -6,25 +6,24 @@ public class Contador {
         System.out.println("Digite o primeiro parametro: ");
         int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parametro: ");
-        int parametroDois= terminal.nextInt();
+        int parametroDois = terminal.nextInt();
 
-        try{
+        try {
             contar(parametroUm, parametroDois);
-        }catch(ParametrosInvalidosException e ){
+        } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
 
     }
 
-    static void contar (int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         if (parametroUm >= parametroDois) {
-           throw new ParametrosInvalidosException();
-        } else{
+            throw new ParametrosInvalidosException();
+        } else {
             int contagem = parametroDois - parametroUm;
             System.out.println("Contando ate " + contagem);
-            for(int contador = 0; contador < contagem;){
-                System.out.println("Imprimindo o numero " + (contador+1));
-                contador ++;
+            for (int contador = 0; contador < contagem; contador++) {
+                System.out.println("Imprimindo o numero " + (contador + 1));
             }
         }
     }
