83b71c23abcee2ba37bea175c21212d82ad45f03
PedrooVi <pedroviniciuscorrea@outlook.com>
2024-06-24 09:57:04 -0300

    Desafio banco

---
+++ Desafios de projetos/Desafio-Dio-Banco/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente pedro = new Cliente();
+        pedro.setNome("Pedro");
+
+        Conta cc = new ContaCorrente(pedro);
+        cc.depositar(100);
+       
+        Conta poupanca = new ContaPoupanca(pedro);
+        cc.transferir(100, poupanca);
+
+
+       cc.imprimirExtrato();
+       poupanca.imprimirExtrato();
+    }
+}
